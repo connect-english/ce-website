089331960a116db28b62de4d8f04d041dcdb8478
connect-english <155808813+connect-english@users.noreply.github.com>
2024-01-07 14:40:36 +0530

    Create start_server.sh

---
+++ scripts/start_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# initial startup by running react-script "start", name process "marketing"
+# --watch watches and restarts if files change
+npm start
+
+# install pm2 globally
+npm install pm2 -g
+
+# start the app with pm2
+pm2 start npm --name "marketing" -- start
+pm2 startup
+pm2 save
+pm2 restart all
+
+# enable and start nginx service
+systemctl enable nginx
+systemctl start nginx
