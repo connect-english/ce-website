b177ebeabf9f417a2563c576ce7b1f99581f1868
connect-english <155808813+connect-english@users.noreply.github.com>
2024-01-07 15:08:52 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /app
+#cd /home/app/
 
 # initial startup by running react-script "start", name process "marketing"
 # --watch watches and restarts if files change
-npm start
+#npm start
 
 # install pm2 globally
-npm install pm2 -g
+#npm install pm2 -g
 
 # start the app with pm2
-pm2 start npm --name "marketing" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+#pm2 start npm --name "marketing" -- start
+#pm2 startup
+#pm2 save
+#pm2 restart all
 
 # enable and start nginx service
 systemctl enable nginx
