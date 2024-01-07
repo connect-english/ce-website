f413621eeded67281e7be1880dd93b79100975ca
connect-english <155808813+connect-english@users.noreply.github.com>
2024-01-07 14:39:13 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# install node and npm
+yum install curl -y
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+yum install nodejs -y
+yum install npm -y
+
+# install nginx and allow HTTP traffic
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+firewall-cmd --permanent --add-service=http
+firewall-cmd --reload
