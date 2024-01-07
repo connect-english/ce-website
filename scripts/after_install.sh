86f3b5bf239aaf3b1443ae9893abd5bfe5b8bc58
connect-english <155808813+connect-english@users.noreply.github.com>
2024-01-07 14:38:36 +0530

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# install dependencies
+npm install
+npm run build
+
+# deploy to Nginx public folder
+cp -r build/* /usr/share/nginx/html
+
+# install pm2 globally
+npm install pm2 -g
